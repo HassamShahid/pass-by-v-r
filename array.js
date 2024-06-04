741fe1275deffeede9fee0b3bcf3c696df04ecd9
HassamShahid <168227550+HassamShahid@users.noreply.github.com>
2024-06-04 17:05:16 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,10 @@
+var studetns = ["Hassam", "Ali", "Gazi"];
+//let searchstudents = studetns.findIndex(findAli);
+//studetns [5] = "Zeeshan" ;
+// studetns.sort();
+studetns. splice(2,1, ... ["Shahid", "Anwar"]);
+console. log(...studetns);
+function findAli(std){
+if(std == "'Ali") return true;
+else return false;
+}
